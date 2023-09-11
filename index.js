cfa4e28edf8d0d8ed40ff1f61c719d16352ec875
Dan Hackworth <danielhackworth515@gmail.com>
2023-09-11 19:48:16 -0400

    add object, add functions, pass first 3 tests

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+const employee = {
+    name: "employeeName",
+    streetAddress: "employeeAddress",
+
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const employeeNew = {...employee};
+    employeeNew[key] = value;
+    return employeeNew;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key] = value;
+    return employee;
+
+}
